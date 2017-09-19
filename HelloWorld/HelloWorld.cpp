234dbfde608f81edabba9e1cc07fbd13b3d6277e
Zhicheng Xu <xuzhicheng331@gmail.com>
2017-09-19 16:57:33 -0700

    initial commit

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,13 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	_sleep(50);
+    return 0;
+}
+
